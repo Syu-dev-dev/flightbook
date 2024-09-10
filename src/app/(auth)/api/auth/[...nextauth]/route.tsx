269a077dc2f1b15eb6690cf21264c355c5b8a473
San Yu <sanyunwe94@gmail.com>
2024-09-10 14:36:39 +0630

    app route error
    fix app route error of next auth

--- src/app/(auth)/api/auth/[...nextauth]/route.tsx
+++ src/app/(auth)/api/auth/[...nextauth]/route.tsx
@@ -1,7 +1,17 @@
-import NextAuth from "next-auth"
-// import GithubProvider from "next-auth/providers/github"
-import {authOptions} from "@/lib/auth"
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export const handler = NextAuth(authOptions)
+// Initialize NextAuth handler with auth options
+const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST, } 
+// Export handler specifically for GET and POST requests
+export const GET = handler;
+export const POST = handler;
+
+// import NextAuth from "next-auth"
+// // import GithubProvider from "next-auth/providers/github"
+// import {authOptions} from "@/lib/auth"
+
+// export const handler = NextAuth(authOptions)
+
+// export {handler as GET, handler as POST, } 
